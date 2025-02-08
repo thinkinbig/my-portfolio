456edaf5574992653b7d5d5c6492d19527feb723
Zeyu Li <go72wog@mytum.de>
2025-02-08 21:17:36 +0100

    remove routes

--- next.config.js
+++ next.config.js
@@ -3,23 +3,7 @@ const nextConfig = {
   images: {
     domains: [], // 如果图片来自外部域名，需要在这里添加
   },
-  async redirects() {
-    return [
-      {
-        source: '/projects/:path*',
-        destination: '/404',
-        permanent: false,
-        has: [
-          {
-            type: 'query',
-            key: 'path',
-            // 更新已定义的项目路由列表
-            not: '^(web-ide|tum-sysprog)$'
-          }
-        ]
-      },
-    ];
-  },
+  // 移除 redirects 配置，我们已经在 [id]/page.tsx 中处理了 404 路由
 }
 
 module.exports = nextConfig
